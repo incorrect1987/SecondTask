d1f2a99461d093682457b747233907d65d53686e
Incorrect <alexeev.andrey1987@gmail.com>
2019-12-03 19:00:08 +0300

    Create .babelrc
    add babel option in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,13 @@ module.exports={
         path: path.resolve(__dirname, './dist'),
         publicPath: './dist'
     },
+    module: {
+        rules: [{
+            test: /\$js./,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
+        }]
+    },
     devServer: {
         overlay: true
     }
