3de5b417aec0d621f4c834004adba711addf7f66
Incorrect <alexeev.andrey1987@gmail.com>
2019-12-03 16:06:09 +0300

    Update webpack.config.js
    add output point

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
+const path = require('path')
 module.exports={
     entry: {
-        app: "./src/index.js"
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: './dist'
     }
 }
