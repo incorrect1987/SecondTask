3978394899a4e64f4c475e54b5ac6a477e4c891b
Incorrect <alexeev.andrey1987@gmail.com>
2019-12-04 23:45:45 +0300

    Install sass loader
    create sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports={
     entry: {
-        app: './src/js/index.js'
+        app: './src/index.js'
     },
     output: {
         filename: '[name].js',
@@ -16,14 +16,32 @@ module.exports={
             exclude: '/node_modules/'
         },{
             test: /\.css$/,
-            loader: 'babel-loader',
             use: [
                  MiniCssExtractPlugin.loader,
                  "css-loader"
              ]
-        }]
+        },{
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                 MiniCssExtractPlugin.loader,
+                 {
+                     loader: 'css-loader',
+                     options: {sourceMap: true}
+                 },{
+                    loader: 'sass-loader',
+                    options: {sourceMap: true}
+                },
+             ]
+        },
+    ]
     },
     devServer: {
         overlay: true
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: "[name].css",
+        })
+      ]
 }
