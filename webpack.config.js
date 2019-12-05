ad39e540752d17859b7fe50d4236311ed748c37d
Incorrect <alexeev.andrey1987@gmail.com>
2019-12-05 17:07:11 +0300

    Create dir static

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,15 @@ module.exports={
         },{
             test: /\.css$/,
             use: [
-                 MiniCssExtractPlugin.loader,
-                 "css-loader"
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {sourceMap: true}
+                }, {
+                   loader: 'postcss-loader',
+                   options: {sourceMap: true, config: {path: 'src/js/postcss.config.js'}}
+               }
              ]
         },{
             test: /\.scss$/,
@@ -28,7 +35,11 @@ module.exports={
                  {
                      loader: 'css-loader',
                      options: {sourceMap: true}
-                 },{
+                 }, {
+                    loader: 'postcss-loader',
+                    options: {sourceMap: true, config: {path: 'src/js/postcss.config.js'}}
+                },
+                 {
                     loader: 'sass-loader',
                     options: {sourceMap: true}
                 },
